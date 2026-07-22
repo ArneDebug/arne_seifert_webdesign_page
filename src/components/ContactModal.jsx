e39261b0e9b3298c407b99b93f72acb9c242c54f
A6000 <90980571+Maxi-Madness@users.noreply.github.com>
2026-07-22 19:43:36 +0200

    ContactModal added

---
+++ src/components/ContactModal.jsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+import gsap from "gsap";
+
+export default function ProjectInquiryModal({ isOpen, onClose }) {
+    
+    const [shouldRender, setShouldRender] = useState(isOpen);
+    const backdropRef = useRef(null);
+    const modalRef = useRef(null);
+
+    useEffect(() => {
+        if (isOpen) {
+            setShouldRender(true);
+    
+            document.body.style.overflow = "hidden";
+    
+            gsap.set(backdropRef.current, {
+            opacity: 0,
+            });
+    
+            gsap.set(modalRef.current, {
+            opacity: 0,
+            scale: 0.9,
+            y: 40,
+            });
+    
+            const tl = gsap.timeline();
+    
+            tl.to(backdropRef.current, {
+            opacity: 1,
+            duration: 0.25,
+            });
+    
+            tl.to(
+            modalRef.current,
+            {
+                opacity: 1,
+                scale: 1,
+                y: 0,
+                duration: 0.5,
+                ease: "power3.out",
+            },
+            "-=0.1"
+            );
+        } else if (shouldRender){
+            const tl = gsap.timeline({
+            onComplete: () => {
+                setShouldRender(false);
+                document.body.style.overflow = "auto";
+            },
+            });
+    
+            tl.to(modalRef.current, {
+            opacity: 0,
+            scale: 0.92,
+            y: 40,
+            duration: 0.4,
+            ease: "power2.in",
+            });
+    
+            tl.to(
+            backdropRef.current,
+            {
+                opacity: 0,
+                duration: 0.3,
+            },
+            "-=0.15"
+            );
+        }
+    }, [isOpen, shouldRender]);
+    
+    if (!shouldRender) return null;
+
+    return (
+        <div
+            ref={backdropRef}
+            className="fixed inset-0 bg-black/70 flex items-center justify-center"
+            onClick={handleClose}
+        >
+            <div
+                ref={modalRef}
+                className="bg-[#161616] rounded-3xl p-10"
+                onClick={(e) => e.stopPropagation()}
+            >
+                <h2>Hello 👋</h2>
+
+                <button onClick={handleClose}>
+                    Close
+                </button>
+            </div>
+        </div>
+    );
+    
+}
